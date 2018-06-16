84c04e9cd11f39bb18f192f2dad555fae4b0916b
Viktor <viktorvelev8@gmail.com>
2018-06-16 22:08:38 +0300

    Dynamic setBase64 function implemented, Basic viewport.

---
+++ matheorems/public/lib/inject.js
@@ -0,0 +1,35 @@
+marginPrecent = 40
+
+viewerSettings = {
+  "appName": "3d",
+  "width": window.outerWidth - window.outerWidth*marginPrecent/100, 
+  "height": window.outerHeight - 60,
+  "showToolBar":false,
+  "borderColor":null,
+  "showMenuBar":false,
+  "showAlgebraInput":false,
+  "customToolbar":"0 || 1",
+  "showResetIcon": false,
+  "enableLabelDrags":false,
+  "enableShiftDragZoom":true,
+  "enableRightClick":false,
+  "capturingThreshold":null,
+  "showToolBarHelp":false,
+  "errorDialogsActive":true,
+  "useBrowserForJS":false,
+}
+
+window.ggbApp = new GGBApplet(viewerSettings, true);
+console.log("здр кп")
+
+window.onload = function() { 
+    window.ggbApp.inject('ggb-element');
+}
+
+window.loadingGGBProbablyDone = function () {
+    console.log("NOW")
+}
+
+window.loadGgbFile = function(ggbFile64) {
+    
+}
