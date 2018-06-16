d2c0102f5a164f8d0e1517681c6d42777b9ed67f
Viktor <viktorvelev8@gmail.com>
2018-06-16 19:51:58 +0300

    Added navbar

--- matheorems/src/index.js
+++ matheorems/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
+import 'semantic-ui-css/semantic.min.css';
 import App from './jsx/App';
 import registerServiceWorker from './registerServiceWorker';
 
--- matheorems/src/jsx/Navbar.jsx
+++ matheorems/src/jsx/Navbar.jsx
@@ -1,15 +1,36 @@
 import React, { Component } from 'react'
+import { Input, Menu } from 'semantic-ui-react'
+import '../styles/Navbar.css'
 
 export default class Navbar extends Component {
-    constructor(props){
-        super(props);
-    }
+    state = { activeItem: 'home' }
 
-    render(){
-        return(
-            <div>
-                Nice
-            </div>
+    handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+
+    render() {
+        const { activeItem } = this.state
+
+        return (
+            <Menu className="navbar">
+                <Menu.Item name='Theorems' active={activeItem === 'Theorems'} onClick={this.handleItemClick} />
+                <Menu.Item
+                    icon='plus'
+                    name='Add'
+                    active={activeItem === 'Add'}
+                    onClick={this.handleItemClick}
+                />
+                <Menu.Header position='center' content="MaTheorems" className="navHead"/>
+                <Menu.Menu position='right'>
+                <Menu.Item>
+                    <Input icon='search' placeholder='Search...' />
+                </Menu.Item>
+                <Menu.Item
+                    name='logout'
+                    active={activeItem === 'logout'}
+                    onClick={this.handleItemClick}
+                />
+                </Menu.Menu>
+            </Menu>
         )
     }
 }
